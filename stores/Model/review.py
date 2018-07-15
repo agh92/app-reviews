cff4edbb4e7c52e63b3834b7ad249f0d0d5f9118
Andres Gil Herrera <agh92@hotmail.com>
2018-07-15 17:15:42 +0200

    get reviews for all country codes ... Still needs to be testend

--- stores/Model/review.py
+++ stores/Model/review.py
@@ -2,15 +2,14 @@ import json
 
 
 class Review:
-    def __init__(self, app_id, review_id, author, date, perma_link, stars, title, body):
-        self.app_id = app_id
+    def __init__(self, app_id, review_id, author, date, stars, title, body):
         self.id = review_id
-        self.author = author
         self.date = date
-        self.perma_link = perma_link
-        self.stars = stars
         self.title = title
         self.body = body
+        self.app_id = app_id
+        self.stars = stars
+        self.author = author
 
     def to_json(self):
         return json.dumps({
@@ -18,8 +17,21 @@ class Review:
             'review_id': self.id,
             'author': self.author,
             'date': self.date,
-            'perma_link': self.perma_link,
             'stars': self.stars,
             'title': self.title,
             'body': self.body
         })
+
+
+class GooglePlayReview(Review):
+
+    def __init__(self, app_id, review_id, author, date, stars, title, body, perma_link):
+        super().__init__(app_id, review_id, author, date, stars, title, body)
+        self.perma_link = perma_link
+
+
+class AppStoreReview(Review):
+
+    def __init__(self, app_id, review_id, author, date, stars, title, body, version):
+        super().__init__(app_id, review_id, author, date, stars, title, body)
+        self.version = version
--- stores/app_store.py
+++ stores/app_store.py
@@ -1,13 +1,85 @@
-
-
-'http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStoreServices.woa/wa/wsLookup?id=343200656&country=us'
 import requests
+from lxml import etree
 
-#${opts.country} ${opts.page} ${id}  sortby={}/${opts.sort}
-_resource = 'https://itunes.apple.com/{}/rss/customerreviews/page={}/id={}/json'
+from . import apple_country_codes
+from .Model.review import AppStoreReview
 
-def reviews(app_id, country):
-    url = _resource.format('de', '1', app_id) #, 'mostRecent'
-    print(url)
-    return requests.post(url)
+# an other posible source:
+# - http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStoreServices.woa/wa/wsLookup?id=343200656&country=us
+# ${opts.country} ${opts.page} ${id}  sortby={}/${opts.sort}
+# use xml because it has two more fields than the json updated and html - json also availible
+_resource = 'https://itunes.apple.com/{}/rss/customerreviews/page={}/id={}/xml'
 
+
+def reviews(app_id, country_code=None):
+    """
+    :param country_code:
+    :param app_id: app id from itunes connect
+    :param country:
+    :return:
+    """
+    r = []
+    codes = apple_country_codes.keys() if country_code is None else [country_code]
+    for code in codes:
+        received = 1
+        page = 1
+        print('Reviews for ', code)
+        while received > 0:
+            url = _resource.format(code, page, app_id)  # , 'mostRecent'
+            rx = _reviews(url)
+            print('Page ', page, ' :', len(rx))
+            r.extend(rx)
+            received = len(rx)
+            page += 1
+    return r
+
+
+def _reviews(url):
+    content = requests.post(url).content
+    tree = etree.fromstring(content)
+    std_namespace = tree.nsmap[None]
+    im_namespace = tree.nsmap['im']
+    r = []
+    # skip the first entry because its the itunes description and not a review
+    first = True
+    iter_child = tree.iter('{' + std_namespace + '}entry')
+    itunes_entry = tree.find('{' + std_namespace + '}entry')
+    if itunes_entry is not None:
+        app_id = itunes_entry.find('{' + std_namespace + '}id').get('{'+im_namespace+'}id')
+    for child in iter_child:
+        if first:
+            next(iter_child)
+            first = False
+        parsed_review = _parse_review(child)
+        parsed_review.app_id = app_id
+        r.append(parsed_review)
+
+    return r
+
+
+def _parse_review(entry, std_namespace='http://www.w3.org/2005/Atom', im_namespace='http://itunes.apple.com/rss'):
+    review_id = entry.find('{' + std_namespace + '}id').xpath("string()")
+    updated = entry.find('{' + std_namespace + '}updated').xpath("string()")
+    #TODO get version for date?
+    title = entry.find('{' + std_namespace + '}title').xpath("string()")  # title <title> text
+    body = None
+    for content in entry.iter('{' + std_namespace + '}content'):  # content < type="text"> text
+        if content.get("type") == 'text':
+            body = content.xpath("string()")
+            break
+        elif content.get("type") == 'html':
+            #TODO
+            pass
+    rating = entry.find('{' + im_namespace + '}rating')  # rating <im:rating> text
+    if rating is not None:
+        rating = rating.xpath("string()")
+    version = entry.find('{' + im_namespace + '}version')  # version <im:version> text
+    if version is not None:
+        version = version.xpath("string()")
+    author = entry.find('{' + std_namespace + '}author')  # author_name <author><name> text
+    author_name = None
+    if author is not None:
+        author_name = author.find('{' + std_namespace + '}name')
+        if author_name is not None:
+            author_name = author_name.xpath("string()")
+    return AppStoreReview(None, review_id, author_name, updated, rating, title, body, version)
