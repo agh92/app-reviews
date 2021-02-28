47f2327ebad248fe0d25fed6b155f9fa88b80092
Andres G <agh92@hotmail.com>
2021-02-28 11:30:34 +0100

    fix wrong names, add output checks and further implementation

--- reviews.py
+++ reviews.py
@@ -1,4 +1,7 @@
+import os
+
 import click
+
 import stores
 from stores.itunes_store import reviews as apple_reviews
 from stores.google_play import reviews as play_reviews
@@ -15,12 +18,13 @@ def cli(verbose):
 @click.option('-id', '--app-id', 'appId', required=True, type=int)
 @click.option('-cc', '--country-code', 'countryCode', required=True, type=click.Choice(code_choises))
 @click.option('-o', '--output', 'output', required=False, type=click.Path())
-def google(appId, countryCode):
+def google(appId, countryCode, output):
     revs = play_reviews(appId, countryCode)
+    # TODO remove print revs method and just write to std-out
     if not output:
         print_revs(revs)
     else:
-        save(revs, path)
+        save(revs, output)
 
 
 @cli.command()
@@ -29,11 +33,14 @@ def google(appId, countryCode):
 @click.option('-o', '--output', 'output', required=False, type=click.Path())
 def apple(appId, countryCode, output):
     revs = apple_reviews(appId, countryCode)
+    if len(revs) == 0:
+        print('No reviews found!')
+        return
     # TODO remove print revs method and just write to std-out
-    if not output:
+    if output is None:
         print_revs(revs)
     else:
-        save(revs, path)
+        save(revs, output)
 
 
 def print_revs(reviews):
@@ -41,20 +48,31 @@ def print_revs(reviews):
         print(review.to_json())
 
 
-def save(reviews, path: click.Path):
-    destination = get_destination(path)
+def save(reviews, output: click.Path):
+    destination = get_destination(output)
     write(reviews, destination)
 
 
-def get_destination(path: click.Path):
-    if path.path_type == 'file':
-        if path.exists:
-            # TODO check if file is emty and use it if not emty throw error
-            pass
+def get_destination(output: click.Path):
+    if output.path_type == 'file':
+        return handle_file(output)
     else:
         # TODO use appId as path name create file
-        pass
+        print('Create file in ' + output.name)
 
 
-def write(reviews, destination):
-    pass
+def handle_file(output: click.Path):
+    if output.exists:
+        if os.stat(output.path).st_size > 0:
+            raise click.BadOptionUsage(
+                'output', 'destination file is not empty!')
+        if output.writable is not True:
+            raise click.BadOptionUsage(
+                'output', 'destination file is not writable!')
+    return open(output.name, 'w')
+
+
+def write(reviews, output):
+    # TODO implement writing
+    print('Write!')
+    print_revs(reviews)
