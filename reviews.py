8c9b0dc777ebb19606ee327f64c2cac028b8816b
Andres G <agh92@hotmail.com>
2021-03-01 19:23:18 +0100

    implement writing to file and fix some click stuff

--- reviews.py
+++ reviews.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import click
 
@@ -15,32 +16,43 @@ def cli(verbose):
 
 
 @cli.command()
-@click.option('-id', '--app-id', 'appId', required=True, type=int)
+@click.option('-id', '--app-id', 'appId', required=True, type=str)
 @click.option('-cc', '--country-code', 'countryCode', required=True, type=click.Choice(code_choises))
-@click.option('-o', '--output', 'output', required=False, type=click.Path())
-def google(appId, countryCode, output):
-    revs = play_reviews(appId, countryCode)
-    # TODO remove print revs method and just write to std-out
-    if not output:
-        print_revs(revs)
-    else:
-        save(revs, output)
+@click.option('-o', '--output', 'output', required=False, type=click.Path(file_okay=True, dir_okay=False, writable=True, resolve_path=True))
+def play(appId, countryCode, output):
+    check_output(output)
+    download_revs(appId, countryCode, play_reviews, output)
 
 
 @cli.command()
 @click.option('-id', '--app-id', 'appId', required=True, type=str)
 @click.option('-cc', '--country-code', 'countryCode', required=True, type=click.Choice(code_choises))
-@click.option('-o', '--output', 'output', required=False, type=click.Path())
-def apple(appId, countryCode, output):
-    revs = apple_reviews(appId, countryCode)
+@click.option('-o', '--output', 'output', required=False, type=click.Path(file_okay=True, dir_okay=False, writable=True, resolve_path=True))
+def itunes(appId, countryCode, output):
+    check_output(output)
+    download_revs(appId, countryCode, apple_reviews, output)
+
+
+def check_output(output):
+    if output:
+        if os.path.isfile(output) and os.stat(output).st_size > 0:
+            raise click.BadOptionUsage(
+                'output', 'destination file is not empty!')
+
+
+def download_revs(appId, countryCode, download_function, output):
+    revs = download_function(appId, countryCode)
     if len(revs) == 0:
         print('No reviews found!')
         return
-    # TODO remove print revs method and just write to std-out
-    if output is None:
+    save(revs, output)
+
+
+def save(reviews, output):
+    if not output:
         print_revs(revs)
-    else:
-        save(revs, output)
+        return
+    write(reviews, output)
 
 
 def print_revs(reviews):
@@ -48,31 +60,7 @@ def print_revs(reviews):
         print(review.to_json())
 
 
-def save(reviews, output: click.Path):
-    destination = get_destination(output)
-    write(reviews, destination)
-
-
-def get_destination(output: click.Path):
-    if output.path_type == 'file':
-        return handle_file(output)
-    else:
-        # TODO use appId as path name create file
-        print('Create file in ' + output.name)
-
-
-def handle_file(output: click.Path):
-    if output.exists:
-        if os.stat(output.path).st_size > 0:
-            raise click.BadOptionUsage(
-                'output', 'destination file is not empty!')
-        if output.writable is not True:
-            raise click.BadOptionUsage(
-                'output', 'destination file is not writable!')
-    return open(output.name, 'w')
-
-
 def write(reviews, output):
-    # TODO implement writing
-    print('Write!')
-    print_revs(reviews)
+    with open(output, 'w') as file:
+        file.write('[' + ','.join([review.to_json()
+                                   for review in reviews]) + ']')
