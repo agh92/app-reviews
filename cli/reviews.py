0de25565c002aa9acbe3101af3ee3f391fab9e27
Andres G <agh92@hotmail.com>
2021-03-07 15:07:50 +0100

    refactor and resolve some TODOs

--- cli/reviews.py
+++ cli/reviews.py
@@ -1,12 +1,10 @@
 import os
 import sys
-from typing import Callable
 from rx import operators as ops
 
 import click
 
 import stores
-from stores.Model.review import Review
 from stores.itunes.App import App
 from stores.google_play.functions import reviews as play_reviews
 from stores import code_choices
@@ -35,7 +33,19 @@ def cli(verbose: bool):
     type=click.Path(file_okay=True, dir_okay=False, writable=True, resolve_path=True),
 )
 def play(app_id: str, country_code: str, output: str):
-    download_reviews(app_id, country_code, play_reviews, output)
+    validate_output(output)
+    reviews = play_reviews(app_id, country_code)
+
+    if len(reviews) > 0:
+        with open(output, "w") if output is not None else sys.stdout as file_handle:
+            print(
+                "[",
+                ",".join([review.to_json() for review in reviews]),
+                "]",
+                file=file_handle,
+            )
+    else:
+        print("No reviews found!")
 
 
 @cli.command()
@@ -55,37 +65,19 @@ def play(app_id: str, country_code: str, output: str):
     type=click.Path(file_okay=True, dir_okay=False, writable=True, resolve_path=True),
 )
 def itunes(app_id: str, country_code: str, output: str):
+    validate_output(output)
     app = App(app_id, country_code)
     with open(output, "w") if output is not None else sys.stdout as file_handle:
         print("[", file=file_handle)
-        app.reviews.pipe(
-            # TODO what if there is only one review
-            # TODO what if after buffering only one remains at the end
-            ops.map(lambda review: review.to_json()),
-            ops.buffer_with_count(2),
-        ).subscribe(
-            on_next=lambda value: print(
-                ",".join([rev for rev in value]), file=file_handle
+        container = []
+        app.reviews.pipe(ops.map(lambda review: review.to_json())).subscribe(
+            on_next=lambda review: container.append(review),
+            on_completed=lambda: print(
+                ",".join([review for review in container]), "]", file=file_handle
             ),
-            on_completed=lambda: print("]", file=file_handle),
         )
 
 
-def download_reviews(
-    app_id: str,
-    country_code: str,
-    download_fn: Callable[[str, str], list[Review]],
-    output: str,
-):
+def validate_output(output):
     if output is not None and os.path.isfile(output) and os.stat(output).st_size > 0:
         raise click.BadOptionUsage("output", "destination file is not empty!")
-
-    reviews = download_fn(app_id, country_code)
-
-    if len(reviews) > 0:
-        with open(output, "w") if output is not None else sys.stdout as file:
-            print(
-                "[", ",".join([review.to_json() for review in reviews]), "]", file=file
-            )
-    else:
-        print("No reviews found!")
